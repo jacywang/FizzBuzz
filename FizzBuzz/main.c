49acc9ec1757fe533a901e4b08bedef4d9b08ac3
Jacy Wang <jacywj@gmail.com>
2015-04-02 23:09:27 -0400

    Initial Commit

---
+++ FizzBuzz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuzz
+//
+//  Created by JIAN WANG on 4/2/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
